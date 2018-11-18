330a5973b114fff4e828db89df12e08f2b8f3353
Kristina Bunnell <mrskbunnell@gmail.com>
2018-11-18 14:26:46 -0500

    adding recipe activity

--- app/src/main/java/com/bonehill/mealnipulate/recipeActivity.kt
+++ app/src/main/java/com/bonehill/mealnipulate/recipeActivity.kt
@@ -4,12 +4,37 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_recipe.*
+import org.jetbrains.anko.startActivity
 
 class recipeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
+
+        setSupportActionBar(toolbar)
+        getSupportActionBar()!!.setDisplayHomeAsUpEnabled(true)
+        getSupportActionBar()!!.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.toolmenu, menu)
+
+        toolbar.setOnMenuItemClickListener { item: MenuItem? ->
+
+            when (item!!.itemId) {
+                R.id.newRecipe -> {
+                    startActivity<recipeActivity>()
+                }
+            }
+            true
+        }
+        return true
     }
 
     companion object {
---
+++ app/src/main/java/com/bonehill/mealnipulate/recipeEdit.kt
@@ -0,0 +1,92 @@
+package com.bonehill.mealnipulate
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [recipeEdit.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [recipeEdit.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class recipeEdit : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var name: String? = null
+
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            name = it.getString("NAME", "")
+
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_edit, container, false)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+           // throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+
+
+        @JvmStatic
+        fun newInstance(name: String) =
+                recipeEdit().apply {
+                    arguments = Bundle().apply {
+                        putString("NAME", name)
+
+                    }
+                }
+    }
+}
